fcb5043d5ac6c18fb3db78d8e173b979c77fb213
xeho91 <xeho91@pm.me>
2024-02-06 17:41:44 +0800

    Update main

--- packages/config/src/main.test.ts
+++ packages/config/src/main.test.ts
@@ -1,8 +1,33 @@
-import { describe, it } from "vitest";
-import { config } from "./main";
+import { LOG_LEVELS } from "@packages/logger";
+import { describe, test } from "vitest";
 
-describe("main", () => {
-	it("works", ({ expect }) => {
-		expect(() => config()).not.toThrowError();
+import { ENVIRONMENTS } from "./environment.js";
+import { loadConfig } from "./main.js";
+
+describe("loadConfig()", () => {
+	test("it succeed when the project workspace is set up correctly", async ({ expect }) => {
+		expect(async () => await loadConfig()).not.toThrow();
+	});
+
+	describe("general:", async () => {
+		const config = await loadConfig();
+
+		test("- 'debug' is boolean", ({ expect }) => {
+			expect(config.DEBUG).toBeTypeOf("boolean");
+		});
+
+		test(`- 'env' is string from enum: [${ENVIRONMENTS}]`, ({ expect }) => {
+			const env = config.NODE_ENV;
+
+			expect(env).toBeTypeOf("string");
+			expect(ENVIRONMENTS).toContain(env);
+		});
+
+		test(`- 'log' is string from enum: [${LOG_LEVELS}]`, ({ expect }) => {
+			const level = config.LOG;
+
+			expect(level).toBeTypeOf("string");
+			expect(LOG_LEVELS).toContain(level);
+		});
 	});
 });
--- packages/config/src/main.ts
+++ packages/config/src/main.ts
@@ -1,4 +1,41 @@
-/** This is a test function located in `@packages/config` */
-export function config() {
-	return "config" as const;
+import { LOG_LEVELS, log, setLoggerLevel } from "@packages/logger";
+import { type Schemas, parseEnv } from "znv";
+import { z } from "zod";
+
+import { loadDotenv } from "./dotenv.js";
+import { ENVIRONMENTS } from "./environment.js";
+
+/** @see {@link CONFIG_SCHEMA} */
+export type EnvironmentVariable = keyof typeof CONFIG_SCHEMA;
+
+export const CONFIG_SCHEMA = {
+	// General
+	DEBUG: z
+		.boolean({
+			description: "Enable debugging mode.",
+		})
+		.default(false),
+
+	LOG: z
+		.enum(LOG_LEVELS, {
+			description: "Log output level.",
+		})
+		.default("silent"),
+
+	NODE_ENV: z.enum(ENVIRONMENTS, { description: "The project environment." }).default("development"),
+} satisfies Schemas;
+
+export type Config = z.infer<z.ZodObject<typeof CONFIG_SCHEMA>>;
+
+/** Load the project configuration via environment variables _(`.env` file)_. */
+export async function loadConfig(): Promise<Config> {
+	await loadDotenv();
+
+	log.trace("Validating the project configuration via environment variables...");
+	const config = parseEnv(process.env, CONFIG_SCHEMA);
+
+	setLoggerLevel(config.LOG);
+
+	log.debug({ config }, "The project project configuration is valid");
+	return config as Config;
 }

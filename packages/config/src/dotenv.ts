e4d4bea89f2122ed233a0c21dcb8fcf0d39b2380
xeho91 <xeho91@pm.me>
2024-02-06 17:28:34 +0800

    Add dotenv module

---
+++ packages/config/src/dotenv.ts
@@ -0,0 +1,36 @@
+import { join } from "node:path";
+
+import { log } from "@packages/logger";
+import { findWorkspaceRootPath } from "@packages/path";
+import dotenv from "dotenv";
+
+export async function findDotenvFilePath(): Promise<URL> {
+	const rootDirPathURL = await findWorkspaceRootPath();
+
+	rootDirPathURL.pathname = join(rootDirPathURL.pathname, ".env");
+
+	log.debug({ dotenvFilePath: rootDirPathURL }, "Dotenv file path");
+	return rootDirPathURL;
+}
+
+export async function loadDotenv() {
+	const dotenvFilePath = await findDotenvFilePath();
+
+	const { error, parsed } = dotenv.config({
+		// biome-ignore lint/complexity/useLiteralKeys: Conflicting with TS config
+		debug: process.env["DEBUG"] !== undefined,
+		path: dotenvFilePath,
+	});
+
+	if (error) {
+		log.fatal({ error }, "DotenvError");
+		throw error;
+	}
+
+	if (parsed) {
+		log.debug({ env: parsed }, "Dotenv content:");
+		return parsed;
+	}
+
+	throw new Error("Nothing has returned from parsing the dotenv file.");
+}

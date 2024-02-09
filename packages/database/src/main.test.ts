28a8754564de98d9b9f7bab0943f19934cd5e72a
xeho91 <xeho91@pm.me>
2024-02-09 11:43:22 +0800

    Remove initial `main` & use package exports with wildcard

--- packages/database/src/main.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from "vitest";
-import { database } from "./main";
-
-describe("main", () => {
-	it("works", ({ expect }) => {
-		expect(() => database()).not.toThrowError();
-	});
-});

d3e2130a9867c31aae8a3ae7a7574f7a8810aada
xeho91 <xeho91@pm.me>
2024-02-11 09:36:04 +0800

    Update tsconfig & remove init src

--- apps/landing/src/main.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it } from "vitest";
-import { landing } from "./main";
-
-describe("main", () => {
-	it("works", ({ expect }) => {
-		expect(() => landing()).not.toThrowError();
-	});
-});
--- apps/landing/src/main.ts
+++
@@ -1,4 +0,0 @@
-/** This is a test function located in `@apps/landing` */
-export function landing() {
-	return "landing" as const;
-}

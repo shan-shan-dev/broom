3a48d045d4a40dbc5012f38544314ef386dd8be8
xeho91 <xeho91@pm.me>
2024-02-20 13:02:09 +0800

    Simplify workflow

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,51 +1,31 @@
 import { resolve } from "node:path";
 
-import { getAppRootPathURL } from "@packages/path/app";
-import { getPackageRootPathURL } from "@packages/path/package";
 import type { StorybookConfig } from "@storybook/svelte-vite";
 
-/**
- * FIXME: I need it to be async, because I can't use top-level await.
- */
-async function createConfig(): Promise<StorybookConfig> {
-	/** @see {@link https://storybook.js.org/docs/configure/typescript#configure-storybook-with-typescript} */
-	return {
-		addons: [
-			"@storybook/addon-links",
-			"@storybook/addon-essentials",
-			"@storybook/addon-designs",
-			"@storybook/addon-interactions",
-			"@chromatic-com/storybook",
-		],
-		docs: {
-			autodocs: "tag",
-		},
-		framework: {
-			name: "@storybook/svelte-vite",
-			options: {},
-		},
-		stories: await getStoriesPathPatterns(),
-		typescript: {},
-	};
-}
-
-async function getStoriesPathPatterns(): Promise<string[]> {
-	const urls = await Promise.all([
-		getPackageRootPathURL("ui"),
-		getAppRootPathURL("client"),
-		getAppRootPathURL("landing"),
-	]);
-
-	return urls.flatMap((url) => {
-		return [
-			resolve(url.pathname, "src", "**", "*.stories.ts"),
-			//
-			resolve(url.pathname, "stories", "**", "*.mdx"),
-		];
-	});
-}
-
-/** FIXME: Ugly workaround, the promise will be resolved anyway, somehow. */
-const config = createConfig();
+/** @see {@link https://storybook.js.org/docs/configure/typescript#configure-storybook-with-typescript} */
+const config: StorybookConfig = {
+	addons: [
+		"@storybook/addon-links",
+		"@storybook/addon-essentials",
+		"@storybook/addon-designs",
+		"@storybook/addon-interactions",
+		"@chromatic-com/storybook",
+	],
+	docs: {
+		autodocs: "tag",
+	},
+	framework: {
+		name: "@storybook/svelte-vite",
+		options: {},
+	},
+	stories: [
+		resolve(__dirname, "..", "apps", "client", "src", "**", "*.stories.ts"),
+		resolve(__dirname, "..", "apps", "ladding", "src", "**", "*.stories.ts"),
+		resolve(__dirname, "..", "packages", "token", "stories", "**", "*.mdx"),
+		resolve(__dirname, "..", "packages", "ui", "src", "**", "*.stories.ts"),
+		resolve(__dirname, "..", "packages", "ui", "stories", "**", "*.mdx"),
+	],
+	typescript: {},
+};
 
 export default config;

d0e1676b74f0bdded6bb81db34f36e76942f2929
xeho91 <xeho91@pm.me>
2024-02-25 21:19:50 +0800

    Add snippets

--- libs/util/src/object.ts
+++ libs/util/src/object.ts
@@ -1,6 +1,7 @@
+import type { Entries } from "type-fest";
+
 /**
  * Snippets related to using JavaScript [Object](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object).
- *
  * @packageDocumentation
  */
 
@@ -10,4 +11,12 @@
  */
 export function typedObjectKeys<T extends object>(object: T) {
 	return Object.keys(object) as (keyof T)[];
+}
+
+/**
+ * Get the typed object entries.
+ * @param object - object from whose you want to return it's entries as typed array.
+ */
+export function typedObjectEntries<T extends object>(obj: T): Entries<T> {
+	return Object.entries(obj) as Entries<T>;
 }

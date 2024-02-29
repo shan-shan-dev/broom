4dcd000390d33c4c91d8d30f135eb60a6c460f72
xeho91 <xeho91@pm.me>
2024-02-29 12:05:42 +0800

    inject into Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,10 @@
+// @ts-ignore - Intentional. To avoid overhead of including all of the apps & libs at the root.
+import "../libs/asset/src/font/mono";
+// @ts-ignore - Intentional. To avoid overhead of including all of the apps & libs at the root.
+import "../libs/asset/src/font/sans";
+// @ts-ignore - Intentional. To avoid overhead of including all of the apps & libs at the root.
+import "../libs/asset/src/font/serif";
+
 import type { Preview } from "@storybook/svelte";
 
 const preview: Preview = {
